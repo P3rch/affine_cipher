e73f200475db84b5329b94e2dc77cd216067cca0
Aneta Kula <aneta.kula97@gmail.com>
2022-08-09 13:00:37 +0200

    Add files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main(){
     cout << "Hello" << endl;
+
+    string text;
+    ifstream Input("text.txt");
+    while (getline (Input, text)) {
+        cout << text;
+    }
+
+    ofstream Output("cipher.txt");
+    Output << "Wrote to a file";
+    Output.close();
+
 }
